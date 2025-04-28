4781e09a8f01cc1be5549e4098edeadf4ce83b62
IsraelCunha1989 <israelcunha89@gmail.com>
2025-04-28 15:29:37 -0300

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -11,7 +11,7 @@ export default function Header() {
   };
   
   return (
-    <header className="bg-white shadow-md">
+    <header className="bg-white shadow-md border-t-4 border-primary">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between h-16">
           <div className="flex">
